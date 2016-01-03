0c275fdaf7537c79e679655efef90633e421c303
Kenneth Jensen <kennethjensenb@gmail.com>
2016-01-03 07:31:31 -0600

    Update AnsiCFizzbuzz.c

--- AnsiCFizzbuzz.c
+++ AnsiCFizzbuzz.c
@@ -1,10 +1,14 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int
 main(void) {
-    int x;
-    for(x=1;x<101;x++) {
+    register char x;
+    for(x = 1; x < 101; ++x) {
         if (x%3==0) printf("Fizz");
         if (x%5==0) printf("Buzz");
         else if (x%3) printf("%d", x);
         printf("\n");
     }
+    return EXIT_SUCCESS
 }
