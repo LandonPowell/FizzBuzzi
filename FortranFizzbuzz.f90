7a4501753dd0936813936aaa4e2e5236070c4950
Landon Powell <landonjpowell@airmail.cc>
2016-03-09 21:16:18 -0500

    Update FortranFizzbuzz.f90

--- FortranFizzbuzz.f90
+++ FortranFizzbuzz.f90
@@ -2,15 +2,15 @@ program FizzBuzz
 
 integer x
 do x = 1, 100, 1
-    if ( modulo(x, 15) == 0 ) then
-        print *, "FizzBuzz"
-    else if ( modulo(x, 3) == 0 ) then
-        print *, "Fizz"
-    else if ( modulo(x, 5) == 0 ) then
-        print *, "Buzz"
-    else 
-        print *, x
-    end if
+   if      ( modulo(x, 15) == 0 ) then
+       print *, "FizzBuzz"
+   else if ( modulo(x,  3) == 0 ) then
+       print *, "Fizz"
+   else if ( modulo(x,  5) == 0 ) then
+       print *, "Buzz"
+   else
+       print *, x
+   end if
 end do
 
-end program FizzBuzz
+end program FizzBuzz 
